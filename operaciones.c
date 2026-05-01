917340359a173a11f802814b8827bdfe331a1252
florenciataborda <ftaborda@frba.utn.edu.ar>
2026-05-01 20:21:19 -0300

    Agrega comentario explicativo en multiplicar

--- operaciones.c
+++ operaciones.c
@@ -12,7 +12,7 @@ int restar(int a, int b) {
     return a - b;
 }
 
-/* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
+/* ── multiplicar dos enteros ─────────────────────── */
 
 int multiplicar(int a, int b) {
    
