88769330feeaa08cfe54332a5412724fbfe84897
aguss06 <anunezaguirre@frba.utn.edu.ar>
2026-05-01 21:49:39 -0300

    Mejora: Agregue la explicacion de como funciona el algoritmo de multiplicacion

--- operaciones.c
+++ operaciones.c
@@ -14,7 +14,7 @@ int restar(int a, int b) {
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
 /*Multiplicar dos enteros a y b*/
-
+/*Calcula el producto de dos enteros a y b utilizando el operador de mulitplicacion*/
 int multiplicar(int a, int b) {
    
     return a * b; /* TODO: implementar */
