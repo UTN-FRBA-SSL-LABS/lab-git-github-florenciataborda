0100da50297b8167c01fb48bc03e57cc273d86b6
florenciataborda <ftaborda@frba.utn.edu.ar>
2026-05-04 10:55:36 -0300

    wip: experimento roto

--- operaciones.c
+++ operaciones.c
@@ -9,7 +9,7 @@ int sumar(int a, int b) {
 /* ── restar — ya implementada ────────────────────────────────────────────── */
 
 int restar(int a, int b) {
-    return a - b;
+    return a - b; //mal aproposito
 }
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
